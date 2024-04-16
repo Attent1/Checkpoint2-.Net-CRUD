a69b05cf516f5348b3a2c1c163c0a6c3092fddf0
Attent1 <rafaeldsc22@outlook.com>
2024-04-16 00:14:56 -0300

    Adicionado validar login

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Checkpont2.Models;
+﻿using Checkpont2.Data;
+using Checkpont2.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,9 +7,31 @@ namespace Checkpont2.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly DataContext _context;
+
+        public LoginController(DataContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult LoginValid(User request)
+        {
+            var user = _context.TB_USERS.FirstOrDefault(x => x.UserEmail == request.UserEmail);
+            if (user == null)
+            {
+                return BadRequest("Email de usuário inexistente");
+            }
+
+            if (user.UserPassword != request.UserPassword)
+            {
+                return BadRequest("Senha incorreta");
+            }
+            
+            return View(user);
         }
 
     }
