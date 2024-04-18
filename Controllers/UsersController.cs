cbc01aab8f387a65a603b585caae0da2bc81e69d
camiped <millinha_smap@hotmail.com>
2024-04-17 22:12:13 -0300

    Criada página para visualizar todos os usuários registrados.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Checkpont2.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Checkpont2.Controllers
+{
+    public class UsersController:Controller
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _context.TB_USERS.ToList();
+            return View(users);
+        }
+    }
+}
